84ca058f7f030e436396ac93b6d0346baf8fd238
Theo Danneels <danneels.theo@gmail.com>
2022-04-28 01:22:52 +0200

    p33

---
+++ README.md
@@ -0,0 +1,28 @@
+## Linker
+lld for linux, zld for mac. Maybe try out mold?
+
+## Watcher
+```sh
+cargo watch -x check -x test -x run
+```
+
+## Code coverage
+```sh
+cargo tarpaulin --ignore-tests
+```
+
+## Linting
+```sh
+cargo clippy -- -D warnings
+```
+
+## Formatting
+```sh
+cargo fmt
+```
+
+## Security Vulnerabilities
+Maybe try cargo-deny?
+```sh
+cargo audit
+```
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,19 @@
 #![warn(clippy::pedantic)]
 #![warn(clippy::nursery)]
 
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::routing::get;
+use axum::{Router, Server};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/health_check", get(|| async {}));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on {}", addr);
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
