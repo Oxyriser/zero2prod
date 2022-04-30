4a10e2ee959d222a584e84147f7d2f31c9a8b471
Theo Danneels <danneels.theo@gmail.com>
2022-05-01 01:24:45 +0200

    p50

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,9 @@
-#![deny(clippy::all)]
-#![warn(clippy::pedantic)]
-#![warn(clippy::nursery)]
-
 use std::net::SocketAddr;
-
-use axum::routing::get;
-use axum::{Router, Server};
+use zero2prod::run;
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/health_check", get(|| async {}));
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("Listening on {}", addr);
-    Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+async fn main() -> hyper::Result<()> {
+    let address = SocketAddr::from(([127, 0, 0, 1], 3000));
+    run(address).await?;
+    Ok(())
 }
