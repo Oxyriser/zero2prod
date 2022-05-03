7c46dc024ecbc7d3a3e9fafbe216f6523390ac90
Theo Danneels <danneels.theo@gmail.com>
2022-05-03 23:37:59 +0200

    p94

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -4,6 +4,7 @@ use axum::extract::{Extension, Form};
 use axum::http::StatusCode;
 use chrono::Utc;
 use serde::Deserialize;
+use tracing::Instrument;
 use uuid::Uuid;
 
 use crate::app::State;
@@ -18,6 +19,17 @@ pub async fn subscribe(
     Form(form): Form<FormData>,
     Extension(state): Extension<Arc<State>>,
 ) -> StatusCode {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name,
+    );
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!("Saving new subscriber details in the database.");
+
     let res = sqlx::query!(
         r#"
         INSERT INTO subscription (id, email, name, subscribed_at)
@@ -29,12 +41,13 @@ pub async fn subscribe(
         Utc::now()
     )
     .execute(&state.db_pool)
+    .instrument(query_span)
     .await;
 
     match res {
         Ok(_) => StatusCode::CREATED,
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!("Failed to execute query: {:?}", e);
             StatusCode::INTERNAL_SERVER_ERROR
         },
     }
